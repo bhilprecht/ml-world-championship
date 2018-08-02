b3ab1b313983e1f025fd68d40029e87367e2492d
nzelot <leontsteiner@gmail.com>
2018-08-02 19:08:24 +0200

    final research finished

--- FIFA_WM_Henrik_Leon.py
+++ FIFA_WM_Henrik_Leon.py
@@ -22,8 +22,11 @@ df_penalties = pd.read_csv('data_prepared/penalties.csv', encoding='iso-8859-1',
 
 
 # # Wie oft hat sich ein zurückliegendes Team den Sieg geholt?
+# * Es werden alle Spiele gezählt, bei denen das zur halbzeit zurückliegende Team den Sieg holt
+# * Spiele mit Verlängerung und Elfmeterschießen wurden aus Mangel eines Zwischenstandes am Spielende vor der verlängerung ausgeschlossen
+# * Spiele die am Ende der ersten Halbzeit unentschieden sind wurden auch nicht gezählt
 
-# In[2]:
+# In[4]:
 
 
 ht_wins_after_residual = 0
@@ -52,11 +55,6 @@ for index, row in df_grouped_events.iterrows():
         continue  
     leading_team_wins+=1
     
-
-
-# In[3]:
-
-
 decision_changed_after_ht = ht_wins_after_residual + at_wins_after_residual
 y = [decision_changed_after_ht, halftime_tie, regular_time_tie, leading_team_wins]
 x = ["Decision changed", "Halftime tie", "Regular time tie", "Leading Team Wins"]
@@ -64,47 +62,25 @@ plt.bar(x = x, height = y)
 
 plt.show()
 
-
-# In[4]:
-
-
 total_matches = decision_changed_after_ht + halftime_tie + regular_time_tie + leading_team_wins
 
+print("Insgesamt konnten " + repr(decision_changed_after_ht) + " von Insgesamt " + repr(total_matches) + " gedreht werden.")
+
+
+# ## Antwort: Von knapp 780 gezählten Spielen werden weniger als 40 Spiele gedreht
+
+# # Zu welcher Spielphase steht der Gewinner fest?
+# * Aufgrund einer häufigen Änderung des Spielmodus innerhalb der verschiedenen WM´s wurden nur die WM´s seit 1986 betrachtet, da sich bei diesen der Spielmodus nur marginal geändert hat
+# * Gruppen- und K.O.-Phase werden separat betrachtet
 
 # In[5]:
 
 
-decision_changed_after_ht
-
-
-# In[6]:
-
-
-total_matches
-
-
-# ## Antwort: Von knapp 780 Spielen werden weniger als 40 Spiele gedreht
-
-# ### Hier wird nicht berücksichtigt: Spiele mit Elfmeterschießen oder Nachspielzeit und es wird nicht zwischen Gruppen- und KO-Phase unterschieden
-
-# # Wann steht der Gewinner fest?
-
-# In[7]:
-
-
 # data preparation: event data since 1986
 df_relevant_events=df_events.loc[(df_events['Year'] >= 1986)]
-
-
-# In[8]:
-
-
 df_relevant_events = df_relevant_events.groupby(["MatchID"]).mean()
 
-
-# In[9]:
-
-
+# decide on group per game
 first_half_decided_group = 0
 second_half_decided_group = 0
 first_half_decided = 0
@@ -135,7 +111,7 @@ for index, row in df_relevant_events.iterrows():
 
 # ## Betrachtung der KO-Phase
 
-# In[10]:
+# In[8]:
 
 
 match_decisions = [first_half_decided, second_half_decided, extra_time_decided, penalty_decided]
@@ -144,10 +120,13 @@ plt.bar(x = match_axis, height = match_decisions)
 
 plt.show()
 
+total_matches_ko = first_half_decided + second_half_decided + extra_time_decided + penalty_decided
+print("Gezählts Spiele: " + repr(total_matches_ko))
+
 
 # ## Betrachtung der Gruppen-Phase
 
-# In[11]:
+# In[9]:
 
 
 match_decisions = [first_half_decided_group, second_half_decided_group]
@@ -155,23 +134,22 @@ match_axis = ["First Half", "Second Half"]
 plt.bar(x = match_axis, height = match_decisions)
 
 plt.show()
+total_matches_group = first_half_decided_group + second_half_decided_group
+print("Gezählte Spiele: " + repr(total_matches_group))
 
 
 # # Vorhersage zu welcher Spielphase sich ein Spiel entscheidet
+# * Hier soll mit einem KNN und einer Classification Decision Tree versucht werden, die ergebnisse von oben vorherzusagen
+# * Die Sinnhaftigkeit der Anwendung ist durchaus fraglich
 
-# In[12]:
+# In[10]:
 
 
 # data prep
 df_relevant_events # events since 1986
 df_relevant_events = df_relevant_events.groupby(["MatchID"]).mean()
-# df_relevant_events
 
-
-# In[13]:
-
-
-# lambda function
+# lambda function to generate the base-truth
 def calculateDecisionPhase(x):
     if x["Home Team Goals"] == x["Half-time Home Goals"] and x["Away Team Goals"] == x["Half-time Away Goals"] and x["Penalty"] == False:
         if x["Home Team Goals"] == x["Away Team Goals"]:
@@ -184,35 +162,30 @@ def calculateDecisionPhase(x):
         return 4
     return 2
 
-
-# In[14]:
-
-
+# lambda function to map the different stages of the cup to a boolean where 0 = group stage; 1 = k.o. stage
 def calculateWorldCupStage(x):
     if x["StageRank"] == 1:
         return 0 # group phase
     return 1 # ko phase
 
-
-# In[15]:
-
-
+# add the base-truth and the new stagerank to the dataframe
 df_relevant_events_truth = df_relevant_events
 df_relevant_events_truth["DecisionPhase"] = df_relevant_events_truth.apply(calculateDecisionPhase, axis=1)
 df_relevant_events_truth["WorldCupStage"] = df_relevant_events_truth.apply(calculateWorldCupStage, axis=1)
 
-# df_relevant_events_truth
-
 
 # ## KNN
 
-# In[16]:
+# In[13]:
 
 
 # Normalization
 from sklearn.preprocessing import MinMaxScaler
-scaler = MinMaxScaler()
+from sklearn.cross_validation import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
 
+scaler = MinMaxScaler()
 
 df_relevant_events_truth[["WorldCupStage", "Home Team Goals", "Half-time Home Goals", "Away Team Goals", "Half-time Away Goals", "ExtraTime", "Penalty"]] = scaler.fit_transform(df_relevant_events_truth[["WorldCupStage", "Home Team Goals", "Half-time Home Goals", "Away Team Goals", "Half-time Away Goals", "ExtraTime", "Penalty"]])
 # Values
@@ -221,20 +194,8 @@ x = np.array(df_relevant_events_truth[["WorldCupStage", "Home Team Goals", "Half
 # Labels
 y = np.array(df_relevant_events_truth["DecisionPhase"]) 
 
-
-# In[17]:
-
-
-# Split data
-from sklearn.cross_validation import train_test_split
+# split the test data
 X_train, X_test, Y_train, Y_test = train_test_split(x, y, test_size=0.3, random_state=101)
-
-
-# In[18]:
-
-
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import accuracy_score
 
 # fit decision tree
 clf = KNeighborsClassifier()
@@ -246,41 +207,22 @@ score = clf.score(X_test, Y_test)
 
 train_score = accuracy_score(clf.predict(X_train), Y_train)
 
-
-# In[19]:
-
-
-train_score
+print("Anzahl der Datensätze: " + repr(len(y)))
+print("Accruacy on Test Data: " + repr(score))
+print("Accruacy on Training Data: " + repr(train_score))
 
 
-# In[20]:
+# In[12]:
 
 
-y_pred
-
-
-# In[21]:
-
-
-score
-
-
-# In[22]:
-
-
-# for comparison also compute accuracy for base model (always output zero)
-y_base_pred = np.full(shape = (len(Y_test)), fill_value=2) # two as most matches are decided in second half 
+# for comparison also compute accuracy for base model (always output 2)
+y_base_pred = np.full(shape = (len(Y_test)), fill_value=2) # two as most matches are decided in second half (see analysis above)
 base_score = accuracy_score(y_base_pred, Y_test)
-
-
-# In[23]:
-
-
-base_score
+print("Base Accruacy: " + repr(base_score))
 
 
 # ## Ergebnis des KNN: 
-# * Knapp 440 Datensätze
+# * Knapp 476 Datensätze
 # * Insgesamt 7 Features (Merkmale des Datensatzes)
 #     * Home Team Goals
 #     * Away Team Goals
@@ -294,7 +236,7 @@ base_score
 
 # ## Decision Tree
 
-# In[24]:
+# In[14]:
 
 
 from sklearn import tree
@@ -310,23 +252,8 @@ score = clf.score(X_test, Y_test)
 
 train_score = accuracy_score(clf.predict(X_train), Y_train)
 
-
-# In[25]:
-
-
-y_pred
-
-
-# In[26]:
-
-
-score
-
-
-# In[27]:
-
-
-train_score
+print("Accruacy bei Test Daten: " + repr(score))
+print("Accruacy bei Trainings Daten: " + repr(train_score))
 
 
 # In[28]:
@@ -358,53 +285,38 @@ graph
 
 # # Erfolgsquote von Strafstößen
 
-# In[29]:
+# In[15]:
 
 
 df_penalties_success_rate = df_events[(df_events.EventType.str.contains("P") == True)]
-
-
-# In[30]:
-
-
 f = {
      'EventType':'count'
     }
 
 df_penalties_success_rate = df_penalties_success_rate[["EventType"]].groupby(["EventType"]).agg(f)
-
-
-# In[31]:
-
-
 df_penalties_success_rate
 
 
-# In[32]:
+# In[16]:
 
 
 df_penalties_MP = df_penalties_success_rate.loc["MP"]
-
-
-# In[33]:
-
-
 df_penalties_P = df_penalties_success_rate.loc["P"]
 
 
-# In[34]:
+# In[17]:
 
 
 df_penalties_MP
 
 
-# In[35]:
+# In[18]:
 
 
 df_penalties_P
 
 
-# In[36]:
+# In[19]:
 
 
 df_penalty_quote = print(float(df_penalties_MP) / df_penalties_P)
@@ -412,7 +324,7 @@ df_penalty_quote = print(float(df_penalties_MP) / df_penalties_P)
 
 # ## Antwort: Nur 6,17 % der Strafstöße werden verschossen
 
-# # General information about Football World Cups
+# # Allgemeine Informationen zur WM
 
 # In[37]:
 
@@ -421,7 +333,7 @@ df_cups.set_index('Year').plot(kind='bar', title="General World Cup Information"
 plt.show()
 
 
-# # Attendance per World Cup
+# # Zuschauerzahlen bei einer WM
 
 # In[38]:
 
@@ -433,7 +345,7 @@ df_cups_attendance['Attendance'] = df_cups_attendance.Attendance.astype(float)
 df_cups_attendance.dtypes
 
 
-# ## Total Attendance per World Cup
+# ## Gesamtzuschauerzahl pro WM
 
 # In[39]:
 
@@ -444,7 +356,7 @@ plt.grid()
 plt.show()
 
 
-# ## Average Attendance per World Cup
+# ## Durchschnittliche Besucherzahl pro WM
 
 # In[40]:
 
@@ -455,7 +367,7 @@ df_avg_attendance.set_index("Year").plot(kind='bar', color="b", title="Average a
 plt.show()
 
 
-# # Player with the most goals across all World Cups
+# # Spieler mit den meisten Toren über alle WM´s hinweg
 
 # In[41]:
 
@@ -479,7 +391,7 @@ df_players_goals.groupby(["Player Name"])
 df_players_goals.dropna()
 
 
-# ## List of top scoring players
+# ## List der Spieler mit den meisten Toren
 
 # In[43]:
 
@@ -497,7 +409,7 @@ df_top14_players['EventType'].sort_values(ascending=False).plot(kind='bar', titl
 plt.show()
 
 
-# # Fun with Penalty data
+# # Spaß mit Elfmeterschießen
 # 
 # The used dataset covers only penalty shoot-outs after regular time. Penalty shoot-outs were introduced in 1972.
 
@@ -512,7 +424,7 @@ df_penalties["Winner Penalty Goals"] = df_penalties["Final Result"].str.split("-
 df_penalties["Loser Penalty Goals"]  = df_penalties["Final Result"].str.split("-").str[1]
 
 
-# ## Penalty overview
+# ## Überblick zu allen Elfmeterschießen
 
 # In[45]:
 
@@ -520,7 +432,7 @@ df_penalties["Loser Penalty Goals"]  = df_penalties["Final Result"].str.split("-
 df_penalties
 
 
-# ## How many matches were dicided after penalty shoot-outs?
+# ## Wie viele Spiele wurden insgesamt durch Elfmeterschießen entschieden?
 
 # In[46]:
 
@@ -530,7 +442,7 @@ df_penalties[['Winner']].count()
 
 # Between 1982 and 2014 took 26 penalty shoot-outs place
 
-# ## How many penalty matches by year?
+# ## Wie viele Elfmeterschießen gab es bei den WM´s der vergangenen Jahre?
 
 # In[47]:
 
@@ -542,7 +454,7 @@ df_penalties_by_year.set_index("Year").plot(kind='bar',color='bgrcmyk', title="P
 plt.show()
 
 
-# ## When decided a penalty shoot-out the world cup winner?
+# ## Wann gab es im WM Finale ein Elfmeterschießen?
 
 # In[48]:
 
@@ -552,7 +464,7 @@ df_penalties[df_penalties['Round'] == 'Final']
 
 # In 1994 won Brazil against Italy with the endresult of 3:2, and in 2006 won Italy against France in the world cup final 5:3.
 
-# ## Which team is the most successful on penalties?
+# ## Welches Team hat die meisten Elfmeterschießen gewonnen?
 
 # In[49]:
 
@@ -569,7 +481,7 @@ df_penalty_winners.set_index("Country").plot(kind="bar", color="g", title="Total
 plt.show()
 
 
-# ## Which team is the less successful on penalty?
+# ## Welches Team schneidet beim Elfmeterschießen am schlechtesten ab?
 
 # In[51]:
 
@@ -586,21 +498,21 @@ df_penalty_loosers.set_index("Country").plot(kind="bar", color="r", title="Total
 plt.show()
 
 
-# # How many Goals per Worldcup?
+# # Wie viele Tore wurden pro WM geschossen?
 
-# In[53]:
+# In[105]:
 
 
 goals_per_match = df_events.groupby(['MatchID'])[['Year', 'Home Team Goals', 'Away Team Goals']].mean()
 goals_per_world_cup = goals_per_match.groupby(['Year'])[['Home Team Goals', 'Away Team Goals']].sum()
 print(goals_per_world_cup.sum(1))
-goals_per_world_cup.sum(1).plot()
+goals_per_world_cup.sum(1).plot(figsize=(17,5))
 plt.show()
 
 
-# # Okay seems like more goals occured lately, so lets check the average goals per game for each world cup
+# # Die Anzahl der Tore hat zugenommen. Trifft das auch auf die Durchschnittliche Anzahl zu?
 
-# In[54]:
+# In[104]:
 
 
 goals_per_match_ratio = df_events.groupby(['MatchID'])[['MatchID', 'Year', 'Home Team Goals', 'Away Team Goals']].mean()
@@ -611,14 +523,13 @@ goals_per_match_ratio = goals_per_match_ratio.rename(columns={'size':'Number of 
 goals_per_match_ratio = goals_per_match_ratio.assign(average = lambda x: (x['MatchID']['Number of Games'].astype(dtype=float) / x['total']['Amount of Goals'].astype(dtype=float)) )
 goals_per_match_ratio = goals_per_match_ratio[['average']].rename(columns={'average':'Average Goals per Game'})
 print(goals_per_match_ratio)
-goals_per_match_ratio.plot(kind='bar')
+goals_per_match_ratio.plot(kind='bar', figsize=(17,5))
 plt.show()
 
 
 # Well it seems that yes, the average number of goals per game increased since 1930 but lately the average was more or less consistent with some peaks in the 00´s
 
-# # Next up we ask ourself: has the goalkeeper ever shot a goal?
-# 
+# # Hat ein Torhüter bei einer WM je ein Tor geschossen?
 
 # In[55]:
 
@@ -628,13 +539,13 @@ goal_keeper_goal = df_events.loc[(df_events['EventType'] == 'G') & (df_events['P
 print(goal_keeper_goal)
 
 
-# # Unfortunately no goal keeper has ever shot a goal during world cup
+# ## Nein
 
-# # Next Question is: Does the team from from the country hosting the event perform better than otherwise?
+# # Spielt der Gastgeber besser als sonst bei WM´s?
 # 
-# # Therefore first take a look at the host and the finals for each world cup
+# ### Dafür soll zuerst der Host und der Weltmeister betrachtet werden
 
-# In[59]:
+# In[21]:
 
 
 df_events2 = pd.read_csv('data_prepared/events2.csv', sep=';').replace(np.nan, '', regex=True)
@@ -652,9 +563,9 @@ host_and_winner = host_and_winner[['Year', 'Host Team Name', 'Champion']]
 host_and_winner
 
 
-# # Ok has the host ever won the finals?
+# # Ok wie oft hat der Host die WM gewonnen?
 
-# In[60]:
+# In[22]:
 
 
 host_won = host_and_winner.loc[(host_and_winner['Host Team Name'] == host_and_winner['Champion'])]
@@ -664,3 +575,114 @@ num_world_cups = len(host_and_winner)
 print('That makes the Host win ' + repr(len(host_won)) + ' of ' + repr(len(host_and_winner)))
 print('Which is a quote of: ' + repr(float(num_host_won)/num_world_cups))
 
+
+# # Wie gut schneiden die Gastgeberländer durchschnittlich in einer WM ab?
+
+# In[82]:
+
+
+df_hosts_list = host_and_winner["Host Team Name"]
+df_year_list = host_and_winner["Year"]
+df_year_list = df_year_list.reset_index()["Year"]
+df_year_list
+
+
+# In[77]:
+
+
+df_hosts_list = df_hosts_list.reset_index()["Host Team Name"]
+df_hosts_list
+
+
+# In[53]:
+
+
+df_events2[['StageRank']].max()
+
+
+# In[54]:
+
+
+df_events2[['StageRank']].min()
+
+
+# In[86]:
+
+
+result_avg_host = []
+result_avg_stage = []
+result_avg_max_stage = []
+result_avg_min_stage = []
+result_stage_host_year = []
+
+total_years = len(df_year_list)
+for country_row in df_hosts_list:
+    stage_count = 0
+    stage_min = 7
+    stage_max = 0
+    for year_row in df_year_list:
+        country_name = country_row
+        year = year_row
+        
+        # begin determining the reached stage of the given host country in the given year
+        df_avg_succ_rate = df_events2.loc[(df_events2['Home Team Name'] == country_name)|(df_events2["Away Team Name"] == country_name)]
+        df_avg_succ_rate = df_avg_succ_rate.loc[(df_avg_succ_rate['Year'] == year)]
+        df_avg_succ_rate = df_avg_succ_rate[['MatchID', 'Year', 'Home Team Name', 'Host Team Name', 'Away Team Name', 'StageRank']]
+        df_avg_succ_rate = df_avg_succ_rate.drop_duplicates()
+        max_stage = df_avg_succ_rate['StageRank'].max()
+        if len(df_avg_succ_rate) == 0:
+            max_stage = 0
+        else:
+            max_stage += 1 # shift the max_stage by one th make room for not participating
+        
+        stage_count += max_stage
+        stage_min = stage_min if max_stage > stage_min else max_stage
+        stage_max = stage_max if max_stage < stage_max else max_stage
+        
+    result_avg_host.append(country_row)
+    result_avg_stage.append(float(stage_count)/total_years)
+    result_avg_max_stage.append(stage_max)
+    result_avg_min_stage.append(stage_min)
+
+np_hosts = np.array(df_hosts_list)
+np_years = np.array(df_year_list)
+for i in range(len(df_year_list)):
+    host = np_hosts[i]
+    year = np_years[i]
+    
+    df_host_year_stages = df_events2.loc[(df_events2['Host Team Name'] == host)&(df_events2["Year"] == year)&((df_events2["Home Team Name"] == host) | (df_events2["Away Team Name"] == host))]
+    reached_stage = df_host_year_stages["StageRank"].max()+1
+    if len(df_host_year_stages) == 0:
+        reached_stage = 0
+    result_stage_host_year.append(reached_stage)
+    
+df_avg_host_performance = pd.DataFrame({ 'Year' : df_year_list, 'Host Country' : result_avg_host, 'Reached Stage': result_stage_host_year,'Avg. Reached Stage' : result_avg_stage, 'Lowest Stage Ever Reached' : result_avg_min_stage, 'Highest Stage Ever Reached' : result_avg_max_stage})#
+
+df_avg_host_performance
+
+
+# In[101]:
+
+
+fig, ax = plt.subplots()
+fig.set_size_inches(17,10)
+index = df_avg_host_performance["Host Country"]
+bar_width = 0.4
+
+reached = ax.bar(index, df_avg_host_performance["Reached Stage"], bar_width, label='Reached Stage')
+avg = ax.bar(index, df_avg_host_performance["Avg. Reached Stage"], bar_width, label='Avg. Reached Stage')
+
+ax.set_xlabel('Host Country')
+ax.set_ylabel('Reached Stage')
+ax.set_title('Vergleich der aktuellen und der durchschnittlichen Team Performance')
+ax.legend()
+
+fig.tight_layout()
+plt.show()
+
+
+# ### Folgendes ergibt sich:
+# * Im Allgemeinen sind die Gastgeberländer auch große Fußballnationen
+# * In der Regel ist das Ergebnis des eines Teams im eigenen Land am Besten
+# * Außname bildet hier Spanien, die in ihrer Heim WM etwas unter dem erwarteten Durchschnitt blieben
+# * Für die WM 2002 wo Südkorea und Japan Gastgeberland waren wurde keine Untersuchung durchgeführt
